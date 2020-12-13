9d1c35f7330f2090fd0310c15c65d776b01cf4b8
Jan <jan.binkowski96@gmail.com>
2020-12-13 12:12:28 +0100

    utils updated

--- notebooks/utils/utils.py
+++ notebooks/utils/utils.py
@@ -1,22 +1,28 @@
-import pandas as pd
 import os
+from IPython.display import display
+
+import pandas as pd
 from tqdm import tqdm
 
-def load_from_dir(path: str, suffix: str) -> list:
+def load_from_dir(path: str, suffix: str, verbose=1) -> list:
     """Function to load all xlsx/csv files from directory"""
     list_of_files = os.listdir(path)
     
-    suffix_files = [file_name for file_name in list_of_files if file_name.endswith(suffix)]    
-    dfs = []
+    suffix_files = [file_name for file_name in list_of_files if file_name.endswith(suffix)]
+    names = [name.split(".")[0] for name in suffix_files]
+    dfs = {}
     
-    for file in tqdm(suffix_files):
+    for file, name in tqdm(zip(suffix_files, names)):
         file_path = os.path.join(path, file)
         if suffix == "xlsx":
-            file = pd.read_excel(file_path, index_col=0)
+            df = pd.read_excel(file_path, index_col=0)
         else:
-            file = pd.read_csv(file_path, index_col=0)
-        dfs.append(file)
+            df = pd.read_csv(file_path, index_col=0)
+        if verbose:
+            display(df.describe(), df.shape)
         
+        dfs.update({name: df})
+    
     return dfs
 
 def save_data(dfs: list, names: list, path: str) -> None:
